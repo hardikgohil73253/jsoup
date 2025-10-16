05227f5c85e7fc21bffcf2041779c2a702396c99
Kahiso <mehdifouzail@gmail.com>
2025-10-15 21:31:43 -0400

    More tests to fix Rename constant MaxAttempts to comply with Java constant naming convention from 'src/main/java/org/jsoup/helper/AuthenticationHandler.java' Fixes #7

--- src/test/java/org/jsoup/helper/AuthenticationHandlerTest.java
+++ src/test/java/org/jsoup/helper/AuthenticationHandlerTest.java
@@ -3,5 +3,5 @@ package org.jsoup.helper;
 public class AuthenticationHandlerTest {
     public static final int MAX_ATTEMPTS = AuthenticationHandler.MAX_ATTEMPTS;
 
-    // tests are in ConnectionTest, ProxyTest. This class just makes the MaxAttempts visible for test.
+    // tests are in ConnectionTest, ProxyTest. This class just makes the MAX_ATTEMPTS visible for test.
 }
